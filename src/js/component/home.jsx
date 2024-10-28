74a0bb066a63f1adfa545305c91bb8c741103f8d
Aless <titifavotto@gmail.com>
2024-10-28 20:48:05 +0000

    casiterminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,57 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
-//create your first component
 const Home = () => {
+	const [task, setTask] = useState("");
+	const [tasks, setTasks] = useState([]);
+
+	function addTask(e) { 
+		if (e.key === "Enter" && task !== "") {
+			setTasks([...tasks, task]);
+			setTask(""); 
+		}
+	};
+
+	function removeTask(id) {
+		setTasks(tasks.filter((item, index) => index !== id));
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="text-center mt-5 title">Pendientes</h1>
+			<div className="card">
+				<div className="input-group">
+					<input
+						onChange={(e) => setTask(e.target.value)}
+						onKeyDown={(e) => addTask(e)}
+						value={task}
+						type="text"
+						className="form-control"
+						aria-label="Sizing example input"
+						aria-describedby="inputGroup-sizing-default"
+						placeholder="Pendientes"
+					/>
+				</div>
+				<div className="ms-3">
+					{tasks.map((item, index) => {
+						return (
+							<div className="d-flex justify-content-between" key={item + index}>
+								<li>{item}</li>
+								<button className="btn me-3" onClick={() => removeTask(index)}>
+									<i className="fas fa-window-close text-danger"></i> 
+								</button>
+							</div>
+						);
+					})}
+				</div>
+				<div className="card-footer">
+					{tasks.length} Tareas
+				</div>
+			</div>
 		</div>
 	);
 };
 
 export default Home;
+
+
